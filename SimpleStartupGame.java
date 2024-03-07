7339557c3ef1cbed398c35217c928b6862a8b780
RomanVReyes <142034738+RomanVReyes@users.noreply.github.com>
2024-03-07 07:04:30 +0000

    ppp

---
+++ SimpleStartupGame.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class SimpleStartupGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        SimpleStartup theStartup = new SimpleStartup();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theStartup.setLocationCells(locations);
+        boolean isAlive = true;
+        while (isAlive) {
+            int guess = helper.getUserInput("Ingrese un número");
+            String result = theStartup.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("Tomaste " + numOfGuesses + " intentos");
+            }
+        }
+    }
+}
+
+class SimpleStartup {
+    private int[] locationCells;
+    private int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+
+    public String checkYourself(int guess) {
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
+
+class GameHelper {
+    public int getUserInput(String prompt) {
+        System.out.print(prompt + ": ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
+
